45be65ccf42454757a99897fd713dce399739881
evgeny <evgeny@158.160.8.209>
2025-01-19 01:20:03 +0300

    начало проекта

---
+++ src/router/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '../pages/Home.vue';
+import Character from '../pages/Character.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/character',
+      name: 'Character',
+      component: Character,
+    },
+  ],
+});
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  
+});
+
+export default store;
