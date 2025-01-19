e72f452baaeeb5df4dd06a846c60c7f39dd72b23
evgeny <evgeny@158.160.8.209>
2025-01-20 01:50:54 +0300

    деплой

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import { createVuePlugin } from 'vite-plugin-vue2';
-export default defineConfig({
-  base: '/list-characters/',
-  plugins: [createVuePlugin()],
-  server: {
-    port: 8080,
-  },
-});
