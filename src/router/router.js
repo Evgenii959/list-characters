824edb1de467821c5c8e3ea3bf94225b8835e0e2
evgeny <evgeny@158.160.8.209>
2025-01-20 01:56:05 +0300

    ред. роут

--- src/router/router.js
+++ src/router/router.js
@@ -9,12 +9,12 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/list-characters/',
       name: 'Home',
       component: Home,
     },
     {
-      path: '/:id',
+      path: '/list-characters/:id',
       name: 'Character',
       component: Character,
     },
